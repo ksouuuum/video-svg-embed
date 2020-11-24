b9950a4ae87246b315022ce1b57d6327c91cba71
ksouuuum <59345545+ksouuuum@users.noreply.github.com>
2020-11-24 15:27:37 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,54 @@
 <script>
 
 var URL = ""
+var ytbcheck = ""
+
+function getId(url) {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const match = url.match(regExp);
+
+    return (match && match[2].length === 11)
+      ? match[2]
+      : null;
+}
 
 function setvideo() {
-	URL = document.getElementById("URL").value;.0
-	URL = URL.substring(URL.indexOf('=') + 1);
-	URL = "https://www.youtube.com/embed/"+URL
+	URL = document.getElementById("URL").value;
+	ytbcheck = URL.includes("https://www.youtube.com/watch?v")
+	if (URL == 0) {
+		document.getElementById("error").innerHTML = "merci de remplir le champ textuel"
+		setTimeout(function(){ 
+				document.getElementById("error").innerHTML = "";
+			}, 5000);
+	} else {
+		if (ytbcheck == true){
+			URL = getId(URL)
+			URL = "https://www.youtube.com/embed/"+URL
+		} else {
+			document.getElementById("error").innerHTML = "merci d'entrer une URL valide"
+			setTimeout(function(){ 
+				document.getElementById("error").innerHTML = "";
+				}, 5000);
+		}
+	}
+}
+
+function dlvideo() {
+	console.log('download')
 }
 
 </script>
 
 <style>
+
+
+	body {
+	padding: 25px;
+	background-color: white;
+	color: black;
+	font-size: 25px;
+	}
+
 	.container { 
 	height: 200px;
 	position: relative;
@@ -62,26 +100,35 @@ function setvideo() {
 
 </style>
 
-<h2>SVG video Embeder</h2>
-
-<svg class="filler">
-	<foreignObject width="300" height="200">
-		<iframe title="embed" xmlns="http://www.w3.org/1999/xhtml"
-		  width="300" height="200"
-		  src={URL}
-		  frameborder="0"></iframe>
-   </foreignObject>
-</svg>
+<body>
 
 
 
-<p></p>
-<div class="container">
-	<div class="center">
-		<input id="URL" placeholder="Youtube video URL" type="text">
-		<button  on:click={setvideo}>Valider</button>
+	<h2>SVG video Embeder</h2>
+
+	<svg class="filler">
 		
-	</div>
-</div>
+		<foreignObject width="300" height="200">
+			<iframe title="embed" xmlns="http://www.w3.org/1999/xhtml"
+			width="300" height="200"
+			src={URL}
+			frameborder="0"></iframe>
+	</foreignObject>
+	</svg>
 
+
+
+	<p></p>
+	<div class="container">
+		<p id="error" style="color:red"></p>
+		<div class="center">
+			<input id="URL" placeholder="Youtube video URL" type="text">
+			<button  on:click={setvideo}>Valider</button>
+			<button  on:click={dlvideo}>Download</button>
+			
+		</div>
+	</div>
+
+	
+</body>
 
